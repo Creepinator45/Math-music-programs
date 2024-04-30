ced08b1895c1419cf14e1214af74af0d6a11c0d4
Creepinator45 <creepinator45@gmail.com>
2024-04-30 14:21:52 -0700

    Cleaned up code

--- Harmonic_Entropy_Matlab_Port.py
+++ Harmonic_Entropy_Matlab_Port.py
@@ -7,36 +7,24 @@ for n in range(1, 51):
         if math.gcd(n,i) == 1:
             f.append(i/n)
 
-
 f.sort()
 f=np.array(f)
-print("f")
-print(f)
 
-centf=f
-
-df = np.zeros(centf.shape)
+df = np.zeros(f.shape)
 df[0]=0
 df[-1]=0
-for i in range(2, np.max(centf.shape)):
-    df[i-1]=-(centf[i] - centf[i-2])/2
-print("df")
-print(df)
+for i in range(1, np.max(f.shape)-1):
+    df[i]=-(f[i+1] - f[i-1])/2
+
 sigma = 0.007
 all = np.linspace(0, 1, 1000)
 p = np.zeros(all.shape, dtype=complex)
-for i in range(1, np.max(p.shape)+1):
-    for j in range(1, np.max(f.shape)+1):
-        pp = df[j-1]*np.exp((-0.5/sigma**2)*(centf[j-1]-all[i-1])**2)/np.sqrt(2*np.pi*sigma)
+for i in range(0, np.max(p.shape)):
+    for j in range(0, np.max(f.shape)):
+        pp = df[j]*np.exp((-0.5/sigma**2)*(f[j]-all[i])**2)/np.sqrt(2*np.pi*sigma)
         if pp != 0:
-            p[i-1] = p[i-1] + pp*np.emath.log(pp)
-
-
+            p[i] = p[i] + pp*np.emath.log(pp)
 
 he=-p
-print("he")
-print(he)
-print("1-abs he")
-print(1-np.abs(he))
 plt.plot(all, 1-np.abs(he))
 plt.show()
