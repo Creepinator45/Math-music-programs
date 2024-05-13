631d30862404b9f5253099228e36c11755a1a305
Creepinator45 <creepinator45@gmail.com>
2024-05-13 10:27:46 -0700

    final code cleanup

--- Harmonic_Entropy_Matlab_Port.py
+++ Harmonic_Entropy_Matlab_Port.py
@@ -1,38 +1,46 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import optimize
 
-def Calculate(ratio: float) -> float:
+def calculate(ratio: float, fareySeries: list[float], dfarey: list[float], sigma: float = 0.007) -> float:
     out = 0
-    for w, v in zip(df, f):
+    for w, v in zip(dfarey, fareySeries):
         pp = w * np.exp((-0.5/sigma**2)*(v - ratio)**2)/np.sqrt(2*np.pi*sigma)
         if pp != 0:
             out = out + pp*np.emath.log(pp)
+
     return 1 - np.abs(out)
 
-#generate Farey series of order n
-f = [1]
-for n in range(1, 51):
-    for i in range(1, n):
-        if math.gcd(n,i) == 1:
-            f.append(i/n)
-f.sort()
-f=np.array(f)
+def generateFarey(order: int) -> tuple[list[float], list[float]]:
+    #generate Farey series of order n
+    f = [1]
+    for n in range(1, order):
+        for i in range(1, n):
+            if math.gcd(n,i) == 1:
+                f.append(i/n)
+    f.sort()
+    f=np.array(f)
 
-#find widths
-df = np.zeros(f.shape)
-df[0]=0
-df[-1]=0
-for i in range(1, len(f)-1):
-    df[i]=-(f[i+1] - f[i-1])/2
+    #find widths
+    df = np.zeros(f.shape)
+    df[0]=0
+    df[-1]=0
+    for i in range(1, len(f)-1):
+        df[i]=-(f[i+1] - f[i-1])/2
+    
+    return f, df
 
-#calculate entropy
-sigma = 0.007
-all = np.linspace(0, 1, 1000)
-he = np.zeros(all.shape, dtype=complex)
-for i, x in enumerate(all):
-    he[i] = Calculate(x)
+def main():
+    f, df = generateFarey(50)
 
-plt.plot(all, he)
-plt.show()
+    #calculate entropy
+    all = np.linspace(0, 1, 1000)
+    he = np.zeros(all.shape, dtype=complex)
+    for i, x in enumerate(all):
+        he[i] = calculate(x, f, df)
+
+    plt.plot(all, he)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
