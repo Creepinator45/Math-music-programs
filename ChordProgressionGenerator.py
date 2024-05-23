767d723bc64899f76dbacee12ad7b818e2a78fcd
Creepinator45 <creepinator45@gmail.com>
2024-05-22 22:45:12 -0700

    Starting implementing UI About to switch program over to Harmonic Entropy

--- ChordProgressionGenerator.py
+++ ChordProgressionGenerator.py
@@ -7,28 +7,56 @@ from itertools import permutations, combinations, zip_longest
 from fractions import Fraction
 import matplotlib.pyplot as plt
 import math
+import typer
 
 s = scamp.Session()
 instrument = s.new_part("Square wave")
 
 maxDenom = 30
 
+#calculate Harmonic Entropy, a measure of interval dissonance based on Shannon Entropy
+def calculateHE(ratio: float, fareySeries: list[float], dfarey: list[float], sigma: float = 0.007) -> float:
+    out = 0
+    for w, v in zip(dfarey, fareySeries):
+        pp = w * np.exp((-0.5/sigma**2)*(v - ratio)**2)/np.sqrt(2*np.pi*sigma)
+        if pp != 0:
+            out = out + pp*np.emath.log(pp)
+
+    return 1 - np.abs(out)
+
+def generateFarey(order: int) -> tuple[list[float], list[float]]:
+    #generate Farey series of order n
+    f = [1]
+    for n in range(1, order):
+        for i in range(1, n):
+            if math.gcd(n,i) == 1:
+                f.append(i/n)
+    f.sort()
+    f=np.array(f)
+
+    #find widths
+    df = np.zeros(f.shape)
+    df[0]=0
+    df[-1]=0
+    for i in range(1, len(f)-1):
+        df[i]=-(f[i+1] - f[i-1])/2
+    
+    return f, df
+
 def ChordDissonance(notes: list[float]) -> float:
     totalDissonance = 0
     numIntervals = 0
     for interval in permutations(notes, 2):
         dyad = Fraction.from_float(interval[0]/interval[1]).limit_denominator(maxDenom).as_integer_ratio()
-        print(dyad)
+        #print(dyad)
         #Harmonicity takes 2 frequencies in simplist form, and outputs a value from 0 to 1 where 0 is most dissonant and 1 is most harmonious. octave is 1, fifth is 0.375, unison is inf, values can be negative
         currentDissonance = pow(barlicity.harmonicity(dyad[0], dyad[1]), 2)
         if math.isinf(currentDissonance):
             currentDissonance = 1
-        print(currentDissonance)
+        #print(currentDissonance)
         totalDissonance += currentDissonance
         numIntervals += 1
     return totalDissonance
-
-print(ChordDissonance([pitch.midi_to_hertz(i) for i in [50, 50+6, 50+12]]))
 
 def Cost(chordProgression: list[float], idealDissonances: list[float], degree: int = 3) -> float:
 
@@ -87,17 +115,21 @@ def PrintStatus(result):
     ax.plot(dissonances)
     plt.show()
     
+def generate(dissonances: list[float], verbose: bool = False):
+    startingValueRange = (70, 70+24)
+    startingValues = np.random.random(15) * (startingValueRange[0]-startingValueRange[1]) + startingValueRange[0]
 
+    if verbose:
+        PrintStatus(startingValues)
+        callback = PrintStatus
+    else:
+        def callback(result): None
+    
+    print("generating...")
+    progression = optimize.minimize(Cost, startingValues, dissonances, method="nelder-mead", callback=callback)
 
-#startingValues = np.array([50, 54, 57, 50, 55, 58, 52, 55, 59, 52, 57, 61, 50, 54, 57])
-#startingValues = np.random.random(15) + startingValues
-startingValueRange = (70, 70+24)
-startingValues = np.random.random(15) * (startingValueRange[0]-startingValueRange[1]) + startingValueRange[0]
+    output = np.reshape(progression.x, (math.floor(len(progression.x)/3), 3))
+    return output
 
-PrintStatus(startingValues)
-idealDissonances = [0, 0.3, 0.6, 1, 0]
-progression = optimize.minimize(Cost, startingValues, idealDissonances, method="nelder-mead", callback=PrintStatus)
-
-print(progression)
-iteration = 0
-PrintStatus(progression.x)
+if __name__ == "__main__":
+    typer.run(generate)
