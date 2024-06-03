d7b57547f3f76368b80077e863f8e36cd74ca4eb
Creepinator45 <creepinator45@gmail.com>
2024-06-03 10:54:14 -0700

    implemented harmonic entropy

--- ChordProgressionGenerator.py
+++ ChordProgressionGenerator.py
@@ -8,6 +8,7 @@ from fractions import Fraction
 import matplotlib.pyplot as plt
 import math
 import typer
+from typing_extensions import Annotated
 
 s = scamp.Session()
 instrument = s.new_part("Square wave")
@@ -43,23 +44,31 @@ def generateFarey(order: int) -> tuple[list[float], list[float]]:
     
     return f, df
 
+#Currently I evaluate chord dissonance using the squared sum of the intervals within the chord
+#There is some online discussion on using voronoi cells to extend harmonic entropy to larger chords, but nothing is concrete enough to use here yet
 def ChordDissonance(notes: list[float]) -> float:
     totalDissonance = 0
     numIntervals = 0
     for interval in permutations(notes, 2):
-        dyad = Fraction.from_float(interval[0]/interval[1]).limit_denominator(maxDenom).as_integer_ratio()
-        #print(dyad)
-        #Harmonicity takes 2 frequencies in simplist form, and outputs a value from 0 to 1 where 0 is most dissonant and 1 is most harmonious. octave is 1, fifth is 0.375, unison is inf, values can be negative
-        currentDissonance = pow(barlicity.harmonicity(dyad[0], dyad[1]), 2)
-        if math.isinf(currentDissonance):
-            currentDissonance = 1
-        #print(currentDissonance)
+        intervalRatio = interval[0]/interval[1]
+        currentDissonance = pow(calculateHE(intervalRatio, farey, dfarey), 2)
+        if currentDissonance == 1:
+            currentDissonance = 0
+            numIntervals -= 1
+        if currentDissonance > 1:
+            raise("invalid dissonance")
         totalDissonance += currentDissonance
         numIntervals += 1
+        print(currentDissonance)
+    totalDissonance = totalDissonance*2/numIntervals
+    print(totalDissonance)
     return totalDissonance
 
+#The cost function being used to evaluate the merit of a chord progression
+#Currently it takes into account how closely the individual chords match the desired dissonance, and penalizes large note jumps in between chords
 def Cost(chordProgression: list[float], idealDissonances: list[float], degree: int = 3) -> float:
 
+    #these coefficients can be tweeked to place higher emphasis on closely matching chord dissonance or on having chords relate to their neighbors
     dissonanceCoeffecient = 1
     distanceCoeffecient = 0.000000001
 
@@ -77,7 +86,7 @@ def Cost(chordProgression: list[float], idealDissonances: list[float], degree: i
         chord = [chordProgression[i*degree + j] for j in range(degree)]
         totalDissonanceCost += pow(ChordDissonance(chord) - idealDissonances[i], 2)
 
-    #calculate distance notes move from their previous chord
+    #calculate squared sum of distance notes move from their previous chord
     totalDistanceCost = 0
     for i in range(len(idealDissonances)):
         if i < 1:
@@ -99,26 +108,15 @@ def PrintStatus(result):
     print(iteration)
     print(result)
     iteration += 1
-    if iteration % 1000 != 1:
-        return
-    result = [(Fraction.from_float(note).limit_denominator(maxDenom)) for note in result]
-    result = [note.numerator/note.denominator for note in result]
-
-    dissonances = []
-    instrument.play_note(None, 1.0, 2.0)
-    for i in range(5):
-        chord = [result[i*3 + j] for j in range(3)]
-        print(chord)
-        instrument.play_chord(chord, 1.0, 2.0)
-        dissonances.append(ChordDissonance(chord))
-    fig, ax = plt.subplots()
-    ax.plot(dissonances)
-    plt.show()
     
-def generate(dissonances: list[float], verbose: bool = False):
-    startingValueRange = (70, 70+24)
-    startingValues = np.random.random(15) * (startingValueRange[0]-startingValueRange[1]) + startingValueRange[0]
+farey, dfarey = generateFarey(30)
 
+def generate(dissonances: list[float], verbose: bool = False, degree: int = 3):
+    startingValueRange = (70, 70+24)
+    startingValues = np.random.random(len(dissonances)*degree) * (startingValueRange[0]-startingValueRange[1]) + startingValueRange[0]
+
+    #sets callback function to print every iteration if verbose, otherwise sets callback function to just return None
+    #I think this is kind of a strange way to do this, but this was the cleanest way I could think to do it to avoid unnecassary global variables and multiple similar minimize function calls
     if verbose:
         PrintStatus(startingValues)
         callback = PrintStatus
@@ -126,10 +124,22 @@ def generate(dissonances: list[float], verbose: bool = False):
         def callback(result): None
     
     print("generating...")
-    progression = optimize.minimize(Cost, startingValues, dissonances, method="nelder-mead", callback=callback)
+    progression = optimize.minimize(Cost, startingValues, (dissonances, degree), method="nelder-mead", callback=callback)
 
-    output = np.reshape(progression.x, (math.floor(len(progression.x)/3), 3))
+    output = np.reshape(progression.x, (math.floor(len(progression.x)/degree), degree))
     return output
 
+def CMDgenerate(dissonances: Annotated[list[float], typer.Argument(min=0, max=1)], verbose: bool = False, degree: int = 3):
+    progression = generate(dissonances, verbose, degree)
+    print(progression)
+    dissonances = []
+    for chord in progression:
+        print(chord)
+        instrument.play_chord(chord, 1.0, 2.0)
+        dissonances.append(ChordDissonance(chord))
+    fig, ax = plt.subplots()
+    ax.plot(dissonances)
+    plt.show()
+
 if __name__ == "__main__":
-    typer.run(generate)
+    typer.run(CMDgenerate)
